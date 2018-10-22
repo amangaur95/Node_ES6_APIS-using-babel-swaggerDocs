4bd7c9d644862a2ed974fd8db92c4bb4bd750869
rajneeshksharma <rajneeshksharmaoffice@gmail.com>
2018-10-22 13:45:37 +0530

    production

--- src/config/db.js
+++ src/config/db.js
@@ -2,4 +2,4 @@ import mongoose from 'mongoose';
 import { getConfig } from './config';
 mongoose.Promise = global.Promise;
 const config = getConfig(process.env.NODE_ENV);
-export const connect = ( ) => mongoose.connect(config.MONGO_URI ,{ useNewUrlParser: true });
+export const connect = ( ) => mongoose.connect(config.MONGO_URI);
